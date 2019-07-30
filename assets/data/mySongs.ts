1dc81a8deb8e3c834482fb3c5a84fc71e2dc6b9d
shahvru <52763776+shahvru@users.noreply.github.com>
2019-07-29 22:06:22 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,21 @@
+import {songInfo} from '../../app/songInfo';
+
+export const song: songInfo[] =
+[
+    {
+        song_name: "I don't care", artist: "Ed Sheeran", genre: "Pop", year: 2019,
+        picture: "assets/images/idc.jpg"
+    },
+    {
+        song_name: "Ok", artist: "Backstreet Boys", genre: "Pop", year: 2019,
+        picture: "assets/images/ok.jpg"
+    },
+    {
+        song_name: "Senorita", artist: "Shawn Mendes", genre: "Pop", year: 2019,
+        picture: "assets/images/senorita.jpg"
+    },
+    {
+        song_name: "No place", artist: "Backstreet Boys", genre: "Pop", year: 2019,
+        picture: "assets/images/noPlace.jpg"
+    }
+]
